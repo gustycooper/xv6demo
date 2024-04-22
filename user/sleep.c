f9b51a3e1405934c464d1db432a4377c27903d56
Gusty Cooper <gusty.cooper@gmail.com>
2024-04-22 14:19:12 -0400

    Add util user programs to repo.

---
+++ user/sleep.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  fprintf(1, "sleep does not work\n");
+  exit(0);
+}
---
+++ user/xargs.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  fprintf(1, "xargs does not work\n");
+  exit(0);
+}
